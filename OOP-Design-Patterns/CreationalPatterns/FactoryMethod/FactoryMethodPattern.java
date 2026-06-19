47264d37835bfa1c6af978fa1127c17aea66eaf5
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-06-19 07:34:50 +0530

    Implement Factory Method pattern for vehicles

---
+++ OOP-Design-Patterns/CreationalPatterns/FactoryMethod/FactoryMethodPattern.java
@@ -0,0 +1,66 @@
+interface Vehicle {
+    String getType();
+}
+
+class Car implements Vehicle {
+    @Override
+    public String getType() {
+        return "Car";
+    }
+}
+
+class Bike implements Vehicle {
+    @Override
+    public String getType() {
+        return "Bike";
+    }
+}
+
+class Truck implements Vehicle {
+    @Override
+    public String getType() {
+        return "Truck";
+    }
+}
+
+abstract class VehicleFactory {
+    abstract Vehicle createVehicle();
+}
+
+class CarFactory extends VehicleFactory {
+    // Write your code here
+    @Override
+    public Vehicle createVehicle() {
+        return new Car();
+    }
+}
+
+class BikeFactory extends VehicleFactory {
+    // Write your code here
+    @Override
+    public Vehicle createVehicle() {
+        return new Bike();
+    }
+}
+
+class TruckFactory extends VehicleFactory {
+    // Write your code here
+    @Override
+    public Vehicle createVehicle() {
+        return new Truck();
+    }
+}
+
+/*
+VehicleFactory carFactory = new CarFactory();
+VehicleFactory truckFactory = new TruckFactory();
+VehicleFactory bikeFactory = new BikeFactory();
+
+Vehicle myCar = carFactory.createVehicle();
+Vehicle myTruck = truckFactory.createVehicle();
+Vehicle myBike = bikeFactory.createVehicle();
+
+myCar.getType();   // "Car"
+myTruck.getType(); // "Truck"
+myBike.getType();  // "Bike"
+*/
